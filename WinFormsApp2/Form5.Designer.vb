3a2d91ea71d01b120de36275ea3e6689f03ad984
princessjammm <jamaevinoque@gmail.com>
2024-01-29 16:33:43 +0700

    hi

---
+++ WinFormsApp2/Form5.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form5
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(22, 82)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 60)
+        Button1.TabIndex = 0
+        Button1.Text = "tile1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(103, 82)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 60)
+        Button2.TabIndex = 1
+        Button2.Text = "tile3"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(184, 82)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 60)
+        Button3.TabIndex = 2
+        Button3.Text = "tile5"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(265, 82)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 60)
+        Button4.TabIndex = 3
+        Button4.Text = "tile7"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(346, 82)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 60)
+        Button5.TabIndex = 4
+        Button5.Text = "tile9"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(427, 82)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 60)
+        Button6.TabIndex = 5
+        Button6.Text = "tile11"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(22, 148)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 60)
+        Button7.TabIndex = 6
+        Button7.Text = "tile2"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(103, 148)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 60)
+        Button8.TabIndex = 7
+        Button8.Text = "tile4"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(184, 148)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 60)
+        Button9.TabIndex = 8
+        Button9.Text = "tile6"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(265, 148)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(75, 60)
+        Button10.TabIndex = 9
+        Button10.Text = "tile8"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(346, 148)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 60)
+        Button11.TabIndex = 10
+        Button11.Text = "tile10"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(427, 148)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(75, 60)
+        Button12.TabIndex = 11
+        Button12.Text = "tile12"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(184, 231)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(75, 60)
+        Button13.TabIndex = 12
+        Button13.Text = "START"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(265, 231)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(75, 60)
+        Button14.TabIndex = 13
+        Button14.Text = "FINISH"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(243, 29)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 14
+        ' 
+        ' Form5
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(533, 303)
+        Controls.Add(Label1)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form5"
+        Text = "Form5"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WinFormsApp2/Form5.vb
@@ -0,0 +1,186 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form5
+    Dim playerPosition As Integer = 1
+    Dim isDone As Boolean = False
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button13.Enabled = False
+        Label1.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button13.Enabled = False
+        Label1.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button1.Enabled = False
+        Button2.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button4.Enabled = False
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button3.Enabled = False
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button4.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button6.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button6.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Button9.Enabled = True
+        Button10.Enabled = True
+        Button8.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Button9.Enabled = True
+        Button10.Enabled = True
+        Button8.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Button11.Enabled = True
+        Button12.Enabled = True
+        Button10.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Button11.Enabled = True
+        Button12.Enabled = True
+        Button10.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+        randomFoot()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Button13.Enabled = True
+        Button12.Enabled = False
+        Button9.Enabled = False
+        Button10.Enabled = False
+        Button11.Enabled = False
+        lastGlas()
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Button13.Enabled = True
+        Button12.Enabled = False
+        Button9.Enabled = False
+        Button10.Enabled = False
+        Button11.Enabled = False
+        lastGlas()
+
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Label1.Text = "You Won!"
+        resetGame()
+
+    End Sub
+
+    Private Sub gameOver()
+        isDone = True
+        If isDone Then
+            Label1.Text = "You Lose!"
+            resetGame()
+        End If
+    End Sub
+
+    Private Sub randomFoot()
+        Dim rando As Integer = GetRandom(1, 3)
+        If rando = 1 Then
+            gameOver()
+        Else
+            Label1.Text = "👍"
+        End If
+
+    End Sub
+    Private Sub lastGlas()
+        Dim rando As Integer = GetRandom(1, 3)
+        If rando = 1 Then
+            gameOver()
+        Else
+            Button14.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub resetGame()
+        Button13.Enabled = True
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+        Button10.Enabled = False
+        Button11.Enabled = False
+        Button12.Enabled = False
+        Button14.Enabled = False
+    End Sub
+
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+        Dim Generator As System.Random = New System.Random()
+        Return Generator.Next(Min, Max)
+    End Function
+
+    Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
